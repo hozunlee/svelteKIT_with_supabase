d40450f1918918b11d0cbe89f6da16b365f813b0
hozunlee <ho2yahh@gmail.com>
2023-02-15 14:38:04 +0900

    fix : bug

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,8 +1,8 @@
-// import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
-// export const load = async (event) => {
-// 	console.log(event);
-// 	const { session } = await getSupabase(event);
-// 	console.log('hi2');
-// 	return { session };
-// };
+export const load = async (event) => {
+	console.log(event);
+	const { session } = await getSupabase(event);
+	console.log('hi2');
+	return { session };
+};
